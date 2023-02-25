45de167e16cd39aa0ce5806cb97cc6a39325ed71
xkrampl <xkrampl@stuba.sk>
2023-02-25 12:02:09 +0100

    string equals

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,18 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner klavesnica = new Scanner(System.in);
+        System.out.println("Hello world");
 
+        String a = "a";
+        String b = "a";
+
+        System.out.println("Zadaj pismeno: ");
+        String c = klavesnica.nextLine();
+
+        System.out.println(a.equals(c));
     }
 }
